dd8c44db283add49622974a098f29ee0bf42a01d
prem-web.dev <a70064182@gmail.com>
2026-04-15 22:09:49 +0530

    Update useStore.ts

--- store/useStore.ts
+++ store/useStore.ts
@@ -2,6 +2,17 @@ import { create } from "zustand";
 
 export const useStore = create((set) => ({
   messages: [],
+  loading: false,
+
   addMessage: (msg) =>
-    set((state) => ({ messages: [...state.messages, msg] }))
+    set((state) => ({ messages: [...state.messages, msg] })),
+
+  updateLastMessage: (content) =>
+    set((state) => {
+      const msgs = [...state.messages];
+      msgs[msgs.length - 1].content = content;
+      return { messages: msgs };
+    }),
+
+  setLoading: (val) => set({ loading: val })
 }));
